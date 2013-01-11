75df721e8963100caa5387c5748707ce97458f33
ryan <ryan@outs.in>
2013-01-11 18:15:03 +1100

    fix /staff

--- feature_server/scripts/staff.py
+++ feature_server/scripts/staff.py
@@ -21,7 +21,7 @@ def staff(connection):
         if connection not in connection.protocol.players:
             admin_message += "\n [A]dmin [G]uard [T]rusted"
         else:
-            connection.send_chat("[A]dmin [G]uard [T]rusted")
+            connection.send_chat("[A]dmin [M]oderator [G]uard [T]rusted")
     return admin_message
 add(staff)
 
